1c4a9c4bd67b438ac4f540bcf7c934d6bb1cef45
Paolo <paologabriel.fernandez@gmail.com>
2024-09-24 23:58:55 -0600

    save1

--- START_Lab_3.py
+++ START_Lab_3.py
@@ -10,7 +10,16 @@ def lab3Question2(decimal_number):
     # Take in an argument of a float (decimal) number.
     # Return "zero" if the number is 0, "positive" if the number is positive, and "negative" if the number is negative
     # Return "invalid" if the input is not a float
-    pass
+    if isinstance(decimal_number, float):
+        if decimal_number > 0:
+            print("positive")
+        else: 
+            print("negative")
+    elif decimal_number == 0:
+            print("zero")
+    else: 
+        print("invalid")
+
 
 def lab3Question3(year):
     # Take in a number that represents a year
